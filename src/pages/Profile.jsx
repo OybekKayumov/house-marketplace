56a4e794c498178cc2ae898829635c638fa0bc3b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-21 20:45:30 +0500

    add user Log In and update profile to show logged user Name, see stored data{object} in broser storage-IndexedDB-firebaseLocalStorage

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getAuth } from 'firebase/auth'
+
 
 function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+
+  useEffect(() => {
+    console.log(auth.currentUser);
+
+    setUser(auth.currentUser);
+  }, [])
+  
   return (
-    <div>Profile</div>
+    user ? <h1>{user.displayName}</h1> : 'Not Logged In'
   )
 }
 
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg' 
 
@@ -23,6 +24,26 @@ function SignIn() {
       [e.target.id]: e.target.value
     }))
   }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+      if (userCredential.user) {
+        navigate('/')
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    
+
+  }
   
   return (
     <>
@@ -33,7 +54,7 @@ function SignIn() {
           </p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input 
             type="email" 
             className='emailInput'
