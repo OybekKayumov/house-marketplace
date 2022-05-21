4d8ff2641e7cb5c7602f7bc76b0777768314e543
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-21 14:05:22 +0500

    add start create routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
