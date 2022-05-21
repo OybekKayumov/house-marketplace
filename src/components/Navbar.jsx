8fb71c294dd4caaf4063b8383510a06230510325
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-21 15:22:26 +0500

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,23 +8,56 @@ function Navbar() {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true
+    }
+  }
+
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
           <li className='navbarListItem' onClick={() => navigate('/')}>
-            <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
-            <p>Explore</p>
+            <ExploreIcon 
+              // fill='#2c2c2c' 
+              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} 
+              width='36px' 
+              height='36px' 
+            />
+            <p 
+              className={pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}
+            >
+              Explore
+            </p>
           </li>
 
           <li className='navbarListItem' onClick={() => navigate('/offers')}>
-            <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
-            <p>Offer</p>
+            <OfferIcon 
+              // fill='#2c2c2c' 
+              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'} 
+              width='36px' 
+              height='36px'
+            />
+            <p 
+              className={pathMatchRoute('/offer') ? 'navbarListItemNameActive' : 'navbarListItemName'}
+            >
+              Offer
+            </p>
           </li>
 
           <li className='navbarListItem' onClick={() => navigate('/profile')}>
-            <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
-            <p>Profile</p>
+            <PersonOutlineIcon 
+              // fill='#2c2c2c' 
+              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} 
+              width='36px' 
+              height='36px' 
+            />
+            <p 
+              className={pathMatchRoute('/profile') ? 'navbarListItemNameActive' : 'navbarListItemName'}
+            >
+              Profile
+            </p>
           </li>
         </ul>
       </nav>      
