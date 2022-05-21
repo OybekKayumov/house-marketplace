8771f45ae6b403df87faa2a74baf3ee3f2edc863
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-21 13:40:53 +0500

    add pages/Explore.jsx

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
