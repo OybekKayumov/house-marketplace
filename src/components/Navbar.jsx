dbec6a16f6cf0c4b8778a7b3f723a8aa64d2d9e5
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-21 15:08:33 +0500

    add navigate= useNavigate and location=useLocation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,24 @@ import { ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
 import { ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
 
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
             <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Explore</p>
           </li>
 
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
             <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Offer</p>
           </li>
 
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
             <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Profile</p>
           </li>
