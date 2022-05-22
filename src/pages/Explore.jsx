a50284704025e0524de5025ef8f86728fa880c39
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-22 12:38:43 +0500

    add Explore page rent and sell

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 function Explore() {
   return (
-    <div>Explore</div>
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
+
+      <main>
+        {/* slider */}
+
+        <p 
+          className='exploreCategoryHeading'
+        >
+          Categories
+        </p>
+
+        <div className='exploreCategories'>
+          <Link to='/category/rent'>
+            <img 
+              src={rentCategoryImage} 
+              alt="rent" 
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for rent</p>
+          </Link>
+
+          <Link to='/category/sell'>
+            <img 
+              src={sellCategoryImage} 
+              alt="sell" 
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for sale</p>
+          </Link>
+
+
+        </div>
+      </main>
+    </div>
   )
 }
 
