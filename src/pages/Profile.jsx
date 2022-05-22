b16cfc1692bd1fc69941e9e16c55ae4cd8125227
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-22 05:45:10 +0500

    add logOut OK

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import { getAuth } from 'firebase/auth'
+import { useNavigate, Link } from 'react-router-dom'
 
 
 function Profile() {
-  const [user, setUser] = useState(null)
-
   const auth = getAuth()
 
-  useEffect(() => {
-    console.log(auth.currentUser);
+  // const [user, setUser] = useState(null)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-    setUser(auth.currentUser);
-  }, [])
+  // useEffect(() => {
+  //   console.log(auth.currentUser);
+
+  //   setUser(auth.currentUser);
+  // }, [])
   
+  const naigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut();
+    naigate('/');
+  }
+ 
   return (
-    user ? <h1>{user.displayName}</h1> : 'Not Logged In'
+    // user ? <h1>{user.displayName}</h1> : 'Not Logged In'
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button className='logOut' type='button' onClick={onLogout}>
+          Logout
+        </button>
+
+      </header>
+    </div>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config.js'
@@ -53,7 +54,8 @@ function SignUp() {
 
       navigate('/')
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      toast.error('Something wrong with registration')
     }
   }
 
