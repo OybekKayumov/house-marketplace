468998162035b76f685894ef2cf275bf0efa6433
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-24 20:08:45 +0500

    add Listing.jsx

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
         </Routes>
         <Navbar />
       </Router>
